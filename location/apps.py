4202dc05057f59a58dcea919d8ee141bf65e0b82
Mahdi <alimohammadloumahdi@gmail.com>
2024-07-03 14:08:40 +0330

    Dockerizing

--- location/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class LocationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'location'
-
-    def ready(self) -> None:
-        import location.signals
