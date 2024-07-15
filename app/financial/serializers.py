38d862cfc09592b7526f7539c563b8771094fd50
Mahdi Alimohammadlou <106872357+MahdiAlimohammadlou@users.noreply.github.com>
2024-07-15 23:37:14 +0330

    Add payoff_price to serializers.py

--- app/financial/serializers.py
+++ app/financial/serializers.py
@@ -6,7 +6,7 @@ class ShippingInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShippingInfo
         fields = [
-         "property", "tax_percentage", "title_deed_fee_percentage", "extra_fee_percentage",
+         "property", "tax_percentage", "title_deed_fee_percentage", "extra_fee_percentage", "payoff_price",
            "total_price", "connections", 
         ]
 
